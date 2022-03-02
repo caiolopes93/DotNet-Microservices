caa434b07387fd1676c484434ad2bd48c1b503ea
Caio Lopes <caiocacilopes@hotmail.com>
2022-03-02 17:53:14 -0300

    ItemsController using ItemsRepository

--- src/Play.Catalog.Service/Repositories/ItemsRepository.cs
+++ src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -7,13 +7,13 @@ using Play.Catalog.Service.Entities;
 
 namespace Play.Catalog.Service.Repositories
 {
-    public class ItemRepository
+    public class ItemsRepository
     {
         private const string collectionName = "items";
         private readonly IMongoCollection<Item> dbCollection;
         private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
-        public ItemRepository()
+        public ItemsRepository()
         {
             //Specify connection string in order to connect to mongoDB
             var mongoClient = new MongoClient("mongodb://localhost:27017");
